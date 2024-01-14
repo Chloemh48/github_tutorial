b03bab3b0eb7485536454d76d0f702ada32fb71b
Chloe Fabro <xfabro@uci.edu>
2024-01-14 12:16:24 -0800

    Update github.py

--- github.py
+++ github.py
@@ -8,13 +8,20 @@
 # for i in range(1, n+1):
 #     print(n,":" , fib(i) )
 
-def sum(n):
-    if n == 0:
-        return n
-    else:
-        return n+sum(n-1)
+# def sum(n):
+#     if n == 0:
+#         return n
+#     else:
+#         return n+sum(n-1)
 
-print('The sum is:',sum(5))
+# print('The sum is:',sum(5))
+
+# def grid_path(n,m):
+#     if n == 1 or m == 1:
+#         return 1
+#     else:
+#         return grid_path(n, m-1) + grid_path(n-1, m)
+# print(grid_path(3, 3))
     
 
 
