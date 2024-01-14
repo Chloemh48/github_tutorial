a13cbd85cc62ada3708cdf250cdb357d6ed38517
Chloe Fabro <xfabro@uci.edu>
2024-01-14 02:46:43 -0800

    Update github.py
    use recursion to a sum nonnegative number

--- github.py
+++ github.py
@@ -1,12 +1,20 @@
-def fib(n):
-    if n < 2:
+# def fib(n):
+#     if n < 2:
+#         return n
+#     else:
+#         return (fib(n-1) + fib(n-2))
+
+# n = 10
+# for i in range(1, n+1):
+#     print(n,":" , fib(i) )
+
+def sum(n):
+    if n == 0:
         return n
     else:
-        return (fib(n-1) + fib(n-2))
+        return n+sum(n-1)
 
-n = 10
-for i in range(1, n+1):
-    print(n,":" , fib(i) )
+print('The sum is:',sum(5))
     
 
 
