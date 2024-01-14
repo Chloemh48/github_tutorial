304abadacaae43ac13865f1a931adaf99fcd82ca
Chloe Fabro <xfabro@uci.edu>
2024-01-14 15:35:37 -0800

    Update github.py

--- github.py
+++ github.py
@@ -25,3 +25,44 @@
     
 
 
+# use recursion to reverse a string
+
+# def reverse(s1):
+#     if s1 == '':
+#         return s1
+#     else:
+#          return  s1[-1] + reverse(s1[:-1])
+#         #   return  reverse(s1[1:]) +s1[0]
+   
+    
+# def main():
+#     s = 'hello'
+#     word=reverse(s)
+#     print(word)
+# main()
+
+# def ispalindrome(s):
+#     # base case
+#     if len(s) <= 0:
+#         return True 
+#     elif s[0] == s[-1]:
+#         return ispalindrome(s[1:-1])
+#     else:
+#         return False
+
+# def main():
+#     print(ispalindrome('yuy'))
+# main()
+
+
+# def covert_to_binary(num):
+#     result = ''
+#     if num == 0:
+#         return ''
+#     else:
+#         result = covert_to_binary((num // 2))+ str(num % 2)
+#     return result
+       
+# def main():
+#     print(covert_to_binary(8))
+# main()
